5bb41a5108526e6716cdcd855b68467b6902fb02
Junyoung <tom9744@gmail.com>
2021-09-30 23:54:20 +0900

    TypeORM 설치 후, AppModule에 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from 'config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JourneysModule } from './journeys/journeys.module';
 import { PhotosModule } from './photos/photos.module';
 
 @Module({
-  imports: [JourneysModule, PhotosModule],
+  imports: [JourneysModule, PhotosModule, TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
